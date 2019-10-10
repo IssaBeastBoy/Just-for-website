8d817f615ba45f1ef0c738e5ec5967d8c65eff6f
g16n3498 <g16n3498@HUG208>
2019-10-10 16:01:52 +0200

    Added nice

--- Just for fun/Jave Script.js
+++ Just for fun/Jave Script.js
@@ -1,20 +1,27 @@
+
 window.onload = function () {
     document.getElementById('Logged_In').style.display = 'none';
+
     document.getElementById('Sign_Up').style.display = 'none';
+
     let loginbtn = document.getElementById('loginbtn');
     let SignUpbtn = document.getElementById('SignUpbtn');
+
     loginbtn.onclick = function () {
         document.getElementById('Logged_In').style.display = 'block';
         document.getElementById('Logged_Out').style.display = 'none';
     };
+
     SignUpbtn.onclick= function(){
         document.getElementById('Logged_In').style.display = 'none';
         document.getElementById('Logged_Out').style.display = 'none';
         document.getElementById('Sign_Up').style.display = 'block';
     };
+
     Back_LogOut.onclick = function(){
         document.getElementById('Logged_In').style.display = 'none';
         document.getElementById('Logged_Out').style.display = 'block';
         document.getElementById('Sign_Up').style.display = 'none';        
     };
+
 };
