54e5e678cc006c869ee12540d51df98993a38906
g16t5351 <g16t5351@HUG60>
2019-10-10 15:32:10 +0200

    Add the java script plus the signup page plus buttons

---
+++ Just for fun/Jave Script.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+    document.getElementById('Logged_In').style.display = 'none';
+    document.getElementById('Sign_Up').style.display = 'none';
+    let loginbtn = document.getElementById('loginbtn');
+    let SignUpbtn = document.getElementById('SignUpbtn');
+    loginbtn.onclick = function () {
+        document.getElementById('Logged_In').style.display = 'block';
+        document.getElementById('Logged_Out').style.display = 'none';
+    };
+    SignUpbtn.onclick= function(){
+        document.getElementById('Logged_In').style.display = 'none';
+        document.getElementById('Logged_Out').style.display = 'none';
+        document.getElementById('Sign_Up').style.display = 'block';
+    };
+    Back_LogOut.onclick = function(){
+        document.getElementById('Logged_In').style.display = 'none';
+        document.getElementById('Logged_Out').style.display = 'block';
+        document.getElementById('Sign_Up').style.display = 'none';        
+    };
+};
